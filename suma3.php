c821af17d43c7d6d7853b741d8c8132fe0d55b5b
manupg3 <man_uh_14@hotmail.com>
2015-08-17 22:35:35 -0300

    primer commit

---
+++ suma3.php
@@ -0,0 +1,22 @@
+
+
+   <html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php 
+      $num1=30;
+      $num2=30;
+	 
+  	  	  
+ $res=$num1+$num2;
+ 
+ 
+ 
+ 
+ echo $res; 
+ echo "<h1>".$res."</h1>"; 
+ ?>
+ </body>
+</html>
